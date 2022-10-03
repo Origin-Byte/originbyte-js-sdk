0a6af8688c8d92216c86fbbcee3712fdd4c4b8b8
OriginByte developer <develop@originbyte.io>
2022-10-03 14:59:35 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/extensions': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        // always try to resolve types under `<root>@types`
+        // directory even it doesn't contain any source code, like `@types/unist`
+
+        alwaysTryTypes: true,
+
+        project: 'path/to/folder',
+      },
+    },
+  },
+};
