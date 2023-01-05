9876bc1d7580ebbe4f7da26bc7022f37bd0c529b
OriginByte developer <develop@originbyte.io>
2023-01-05 10:05:45 +0200

    Fix lint

--- src/client/helpers.ts
+++ src/client/helpers.ts
@@ -1,6 +1,6 @@
-import { ArtNftRpcResponse } from './types';
-import { ArtNftParser } from './parsers';
+import { ArtNftRpcResponse } from "./types";
+import { ArtNftParser } from "./parsers";
 
 export const isArtNft = (obj: any): obj is {data: {fields: ArtNftRpcResponse, type: string}} => {
-  return ('data' in obj && 'fields' in obj.data && obj.data.type && !!obj.data.type.match(ArtNftParser.regex));
+  return ("data" in obj && "fields" in obj.data && obj.data.type && !!obj.data.type.match(ArtNftParser.regex));
 };
