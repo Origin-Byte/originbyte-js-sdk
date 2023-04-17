5e8e9fadd8c9d413c32646db1d608b4e97424578
oribyts <oribyts@gmail.com>
2023-04-17 15:11:58 +0300

    update examples/common - switch to testnet + change contract

--- examples/common.ts
+++ examples/common.ts
@@ -8,7 +8,7 @@ import { NftClient, OrderbookFullClient, SafeFullClient } from "../src";
 
 // export const mnemonic = 'muffin tuition fit fish average true slender tower salmon artist song biology';
 export const mnemonic =
-  "strike silk season point pass raven vote hawk hand relief diagram person";
+  "risk pride purity oxygen donor half spray already voice limb table toe";
 
 /**
  * Created Objects:
@@ -20,32 +20,32 @@ export const mnemonic =
 // Step 0 - Publish
 
 export const PACKAGE_OBJECT_ID =
-  "0x30ac932177f6c7bb1ee142838f0faa8b0ac65f250455567c761c39d84c02082d"; // Change to your deployed contract
+  "0x86ed6bc882fa476f20db8d21256a20cc7c841b9e1a37c356daa5406f92412f3c"; // Change to your deployed contract
 export const COLLECTION_ID =
-  "0x8be1aceaa7d4ebf1db11382d4ca1c4c8a358fe08771244f85ce62202ec28b68f"; // Change to your deployed contract
-export const COLLECTION_ID_NAME = `${COLLECTION_ID}::suitraders::SUITRADERS`;
+  "0xfc8ebeab5d39266393b21144ee9a160ef77a2f9eade879defc134b6a8404a532"; // Change to your deployed contract
+export const COLLECTION_ID_NAME = `${COLLECTION_ID}::suitraders::Suitrader`;
 export const COLLECTION_TYPE = `${PACKAGE_OBJECT_ID}::collection::Collection<${COLLECTION_ID_NAME}>`;
 export const NFT_TYPE = `${PACKAGE_OBJECT_ID}::nft::Nft<${COLLECTION_ID_NAME}>`;
 
-export const MINT_CAP_ID = "0x2ac4bb34ae39625385d0d0355f5ff82b53df1db1"; // Change to your deployed contract
+export const MINT_CAP_ID = "0xbb1b98c0955602208b69f1522eaa5eb3efc7e507841cd31d225330a0bea2af34"; // Change to your deployed contract
 // Step 1 - create Flat fee
 export const FEE_OBJECT_ID = "0x2a78356fdb1d91dca38b2ab47082aca02bfd4efb"; // Change to your deployed contract
 // Step 2 - Init marketplace
-export const MARKETPLACE_ID = "0x717aae68dfc03479818c2c550832c6d2668fe87d"; // Change to your deployed contract
+export const MARKETPLACE_ID = "0xe109724b4ef1aa45ddd05aca9a4a7b2f5dcd392791715c606ea97e6bb6a4f665"; // Change to your deployed contract
 // Step 3 - Init Listing
 export const LISTING_ID =
-  "0x5cceb918d7f98f5536c8727b1165f8130633c13f19d7e1b462245ec9a8a3a856"; // Change to your deployed contract
+  "0xbb2809d83eb249b761f09d4b23456702433efbcdef844fa9c1a56aab11c02a4e"; // Change to your deployed contract
 // Step 4 - Create Warehouse
 export const WAREHOUSE_ID = "0xa3f7cfc3124b7e65de6a0f9444b5299f2691f0ab"; // Change to your deployed contract
 // Step 5 - Add inventory to listing
-export const INVENTORY_ID = "0x940bf37390b0b5a7ce10d0b433e39faddae3d136"; // Change to your deployed contract
+export const INVENTORY_ID = "0x79dc9acad51383d31b0777414f611c697614dd74797f43414720f9d097124774"; // Change to your deployed contract
 // Step 6 - Create Market
 export const VENUE_ID =
-  "0x52c63cb8018fecbc2c3ce8806757d79f27569001f182c3eabfa1c59e4abbcd05"; // Change to your deployed contract
+  "0xd3c195dd21a25bec8af0d16a3775ef6e0a9a8137dbb8a560eb473ac961ce1a79"; // Change to your deployed contract
 // Step 7 - Create Orderbook
-export const ORDERBOOK_ID = "0x12f05d1dfc8210877ca83ea5b58f2beef992f587"; // Change to your deployed contract
+export const ORDERBOOK_ID = "0x0edf2669440bd70c2362b63dfcdc86ba45a685e92c752cfe9bc200d90e15ef22"; // Change to your deployed contract
 // Step 8 - Create AllowList
-export const ALLOW_LIST_ID = "0x20c97662993a9ef4ab3b07dd9ed97be182b0ad40"; // Change to your deployed contract
+export const ALLOW_LIST_ID = "0x1b9036d1d951562f6f9f5594eab874c40a466552dd4f1ee3558aa556b677c925"; // Change to your deployed contract
 
 export function normalizeMnemonics(mnemonics: string): string {
   return mnemonics
@@ -59,7 +59,11 @@ export const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
 
 export const provider = new JsonRpcProvider(
   // new Connection({ fullnode: "https://fullnode.devnet.sui.io" })
-  new Connection({ fullnode: "https://explorer-rpc.devnet.sui.io/" })
+  // new Connection({ fullnode: "https://explorer-rpc.devnet.sui.io/" })
+    // new Connection({ fullnode: "https://clutchy.io/testnet-rpc" })
+  new Connection({ fullnode: "https://fullnode.testnet.sui.io" })
+
+
 );
 export const signer = new RawSigner(keypair, provider);
 export const client = new NftClient(provider);
